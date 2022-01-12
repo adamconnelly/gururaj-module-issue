63f80f6037342be0271ce1792302f2f4a8c5b334
Adam Connelly <adam.rpconnelly@gmail.com>
2022-01-12 15:08:12 -0500

    Changes to make module work correctly

---
+++ terraform-modules/aws-modules/vpc/examples/simple-example/main.tf
@@ -0,0 +1,4 @@
+# AC: In this test case we're just instantiating our module to test using it
+module "example" {
+  source = "../../"
+}
--- terraform-modules/aws-modules/vpc/main.tf
+++ terraform-modules/aws-modules/vpc/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_vpc" "main" {
@@ -7,10 +7,13 @@ resource "aws_vpc" "main" {
 }
 
 module "gurus_webserver" {
-    source = "../../terraform/aa-dev"
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.0.0/16"
-    webserver_name = "Guru"
-    ami = "ami-08e4e35cccc6189f4"
-    instance_type = "t2.micro"
+  # AC: Reference your other module via the Spacelift module registry
+  source  = "spacelift.io/adamconnelly/aa-dev/gururaj"
+  version = "0.0.1"
+
+  vpc_id         = aws_vpc.main.id
+  cidr_block     = "10.0.0.0/16"
+  webserver_name = "Guru"
+  ami            = "ami-08e4e35cccc6189f4"
+  instance_type  = "t2.micro"
 }
