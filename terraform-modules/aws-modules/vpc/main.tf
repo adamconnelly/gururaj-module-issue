b378e1619dc1799092035267aae107bf99a45f7b
Adam Connelly <adam.rpconnelly@gmail.com>
2022-01-20 10:28:05 +0000

    Try adding terraform_versions to module config
    Also remove the pinned version from the dependent module.

--- terraform-modules/aws-modules/vpc/main.tf
+++ terraform-modules/aws-modules/vpc/main.tf
@@ -8,8 +8,7 @@ resource "aws_vpc" "main" {
 
 module "gurus_webserver" {
   # AC: Reference your other module via the Spacelift module registry
-  source  = "spacelift.io/adamconnelly/aa-dev/gururaj"
-  version = "0.0.2"
+  source = "spacelift.io/adamconnelly/aa-dev/gururaj"
 
   vpc_id         = aws_vpc.main.id
   cidr_block     = "10.0.0.0/16"
